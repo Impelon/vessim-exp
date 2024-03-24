238da0a4a2a084e0479702664641a61b5e14a378
Impelon <ovi.tatar@googlemail.com>
2024-03-24 20:22:07 +0100

    attempt to replace redis with sqlite
    Kinda works, but there are some issues still. Biggest bummer is that 
    multi-threading support does not translate into multi-process support, 
    and thus to avoid conflicts, timeouts have to be set high and in-memory 
    databases cannot be used. There is aparently some quirk that also 
    prevents some events from processing timely?

--- vessim/sil.py
+++ vessim/sil.py
@@ -8,18 +8,18 @@ from __future__ import annotations
 import json
 import multiprocessing
 import pickle
+import sqlite3
+from contextlib import contextmanager
+from pathlib import Path
 from collections import defaultdict
 from datetime import datetime, timedelta
 from threading import Thread
 from time import sleep
 from typing import Any, Optional, Callable
 
-import docker  # type: ignore
 import pandas as pd
-import redis
 import requests
 import uvicorn
-from docker.models.containers import Container  # type: ignore
 from fastapi import FastAPI
 from loguru import logger
 from requests.auth import HTTPBasicAuth
@@ -28,31 +28,100 @@ from vessim.cosim import Controller, Microgrid
 from vessim.signal import Signal
 from vessim._util import DatetimeLike
 
-
 class Broker:
-    def __init__(self):
-        self.redis_db = redis.Redis()
+    def __init__(self, db_file: str = None):
+        if db_file is None:
+            # NOTE: An in-memory database will not work across multiple processes, only multiple threads.
+            # As such, a temporary file is the only real alternative.
+            db_file = Path("/tmp") / "vessim-sil-{:x}.db".format(id(self))
+        self.db_file = Path(db_file).resolve()
+        # TODO Possibly check concurreny level of sqlite3.
+        # See: https://docs.python.org/3.10/library/sqlite3.html#sqlite3.threadsafety
+        self._initialize_tables()
 
-    def get_microgrid(self) -> Microgrid:
-        return pickle.loads(self.redis_db.get("microgrid"))  # type: ignore
+    @contextmanager
+    def open_db(self):
+        connection = sqlite3.connect(self.db_file, isolation_level="DEFERRED", timeout=30)
+        try:
+            yield connection
+            connection.commit()
+        except Exception as e:
+            connection.rollback()
+            raise e
+        finally:
+            connection.close()
 
-    def get_actor(self, actor: str) -> dict:
-        return json.loads(self.redis_db.get("actors"))[actor]  # type: ignore
+    def _initialize_tables(self):
+        with self.open_db() as c:
+            tables = [ # table name, custom table columns
+                ("microgrids", "microgrid pickleblob"),
+                ("actor_infos", "name TEXT, info pickleblob"),
+                ("p_deltas", "p_delta REAL"),
+                ("events", "category TEXT, value pickleblob"),
+            ]
+            indices = [ # index name, name of affected table, columns to index
+                ("idx_name", "actor_infos", "name"),
+            ]
+            # An extra column for the timestamp as an integer is used for fast queries.
+            for definition in tables:
+                indices.append(("idx_unixts", definition[0], "unixts DESC"))
 
-    def get_p_delta(self) -> float:
-        return float(self.redis_db.get("p_delta"))  # type: ignore
+            for definition in tables:
+                c.execute("CREATE TABLE IF NOT EXISTS {} (unixts INT, exactts TEXT, {});".format(*definition))
+            for definition in indices:
+                c.execute("CREATE INDEX IF NOT EXISTS {} ON {} ({});".format(*definition))
+
+    def _fetch_obj(self, column_spec: str, from_spec: str, converter: Optional[Callable[..., Any]] = None) -> Optional[Any]:
+        # NOTE: Parameter substitution can unfortunately not be used to specify column or table names.
+        with self.open_db() as c:
+            result = c.execute("SELECT {} FROM {} ORDER BY unixts DESC LIMIT 1;".format(column_spec, from_spec)).fetchone()
+        if result is None or converter is None:
+            return result
+        return converter(*result)
+
+    def get_microgrid(self) -> Optional[Microgrid]:
+        return self._fetch_obj("microgrid", "microgrids", pickle.loads)  # type: ignore
+
+    def get_actor(self, actor: str) -> Optional[dict]:
+        return self._fetch_obj("info", "actor_infos WHERE name = {}".format(actor), pickle.loads)  # type: ignore
+
+    def get_p_delta(self) -> Optional[float]:
+        return self._fetch_obj("p_delta", "p_deltas", float)  # type: ignore
 
     def set_event(self, category: str, value: Any) -> None:
-        self.redis_db.lpush(
-            "set_events",
-            pickle.dumps(
-                dict(
-                    category=category,
-                    time=datetime.now(),
-                    value=value,
-                )
-            ),
-        )
+        time = datetime.now()
+        params = (int(time.timestamp()), time.isoformat())
+        with self.open_db() as c:
+            c.execute("INSERT INTO events VALUES (?,?,?,?)", params + (category, pickle.dumps(value)))
+
+    def _add_microgrid_data(self, time: datetime, data: dict) -> None:
+        params = (int(time.timestamp()), time.isoformat())
+        with self.open_db() as c:
+            import sys
+            if "microgrid" in data:
+                c.execute("INSERT INTO microgrids VALUES (?,?,?)", params + (pickle.dumps(data["microgrid"]),))
+            if "actor_infos" in data:
+                for name, info in data["actor_infos"].items():
+                    c.execute("INSERT INTO actor_infos VALUES (?,?,?,?)", params + (name, pickle.dumps(info)))
+            if "p_delta" in data:
+                c.execute("INSERT INTO p_deltas VALUES (?,?,?)", params + (data["p_delta"],))
+
+    def _consume_events(self) -> Iterable[dict]:
+        results = []
+        with self.open_db() as c:
+            c = c.execute("BEGIN TRANSACTION;")
+            results = c.execute("SELECT exactts, category, value FROM events;").fetchall()
+            c.execute("DELETE FROM events;")
+        for result in results:
+            yield {
+                "time": datetime.fromisoformat(result[0]),
+                "category": result[1],
+                "value": pickle.loads(result[2]),
+            }
+
+    def finalize(self) -> None:
+        if str(self.db_file).startswith("/tmp"):
+            self.db_file.unlink(missing_ok=True)
 
 
 class SilController(Controller):
@@ -75,59 +144,52 @@ class SilController(Controller):
         self.api_port = api_port
         self.request_collector_interval = request_collector_interval
         self.kwargs = kwargs
-        self.redis_docker_container = _redis_docker_container()
-        self.redis_db = redis.Redis()
+        self.broker = Broker()
 
         self.microgrid: Optional[Microgrid] = None
 
     def start(self, microgrid: Microgrid) -> None:
         self.microgrid = microgrid
+        name = "Vessim API for microgrid {:x}".format(id(self.microgrid))
 
         multiprocessing.Process(
             target=_serve_api,
-            name="Vessim API",
+            name=name,
             daemon=True,
             kwargs=dict(
                 api_routes=self.api_routes,
                 api_host=self.api_host,
                 api_port=self.api_port,
+                broker=self.broker,
                 grid_signals=self.grid_signals,
             ),
         ).start()
-        logger.info("Started SiL Controller API server process 'Vessim API'")
+        logger.info("Started SiL Controller API server process '{}'", name)
 
         Thread(target=self._collect_set_requests_loop, daemon=True).start()
 
     def step(self, time: datetime, p_delta: float, actor_infos: dict) -> None:
-        pipe = self.redis_db.pipeline()
-        pipe.set("time", time.isoformat())
-        pipe.set("p_delta", p_delta)
-        pipe.set("actors", json.dumps(actor_infos))
         assert self.microgrid is not None
-        pipe.set("microgrid", self.microgrid.pickle())
-        pipe.execute()
+        self.broker._add_microgrid_data(time, {
+            "microgrid": self.microgrid,
+            "actor_infos": actor_infos,
+            "p_delta": p_delta,
+        })
 
-    def finalize(self) -> None:
-        if self.redis_docker_container is not None:
-            self.redis_docker_container.stop()
-        logger.info("Shut down Redis docker container")
+    def finalize(self) -> None: # TODO-now stop daemon processes?
+        self.broker.finalize()
 
     def _collect_set_requests_loop(self):
         while True:
-            events = self.redis_db.lrange("set_events", start=0, end=-1)
-            assert events is not None
-            if len(events) > 0:  # type: ignore
-                events = [pickle.loads(e) for e in events]  # type: ignore
-                events_by_category = defaultdict(dict)
-                for event in events:
-                    events_by_category[event["category"]][event["time"]] = event["value"]
-                for category, events in events_by_category.items():
-                    self.request_collectors[category](
-                        events=events_by_category[category],
-                        microgrid=self.microgrid,
-                        kwargs=self.kwargs,
-                    )
-            self.redis_db.delete("set_events")
+            events_by_category = defaultdict(dict)
+            for event in self.broker._consume_events():
+                events_by_category[event["category"]][event["time"]] = event["value"]
+            for category, events in events_by_category.items():
+                self.request_collectors[category](
+                    events=events_by_category[category],
+                    microgrid=self.microgrid,
+                    kwargs=self.kwargs,
+                )
             sleep(self.request_collector_interval)
 
 
@@ -135,49 +197,14 @@ def _serve_api(
     api_routes: Callable,
     api_host: str,
     api_port: int,
+    broker: Broker,
     grid_signals: dict[str, Signal],
 ):
     app = FastAPI()
-    api_routes(app, Broker(), grid_signals)
+    api_routes(app, broker, grid_signals)
     config = uvicorn.Config(app=app, host=api_host, port=api_port, access_log=False)
     server = uvicorn.Server(config=config)
     server.run()
-
-
-def _redis_docker_container(
-    docker_client: Optional[docker.DockerClient] = None, port: int = 6379
-) -> Container:
-    """Initializes Docker client and starts Docker container with Redis."""
-    if docker_client is None:
-        try:
-            docker_client = docker.from_env()
-        except docker.errors.DockerException as e:  # type: ignore
-            raise RuntimeError("Could not connect to Docker.") from e
-    try:
-        container = docker_client.containers.run(
-            "redis:latest",
-            ports={"6379/tcp": port},
-            detach=True,  # run in background
-        )
-    except docker.errors.APIError as e:  # type: ignore
-        if e.status_code == 500 and "port is already allocated" in e.explanation:
-            # TODO prompt user to automatically kill container
-            raise RuntimeError(
-                f"Could not start Redis container as port {port} is "
-                f"already allocated. Probably a prevois execution was not "
-                f"cleaned up properly by Vessim."
-            ) from e
-        raise
-
-    # Check if the container has started
-    while True:
-        container_info = docker_client.containers.get(container.name)  # type: ignore
-        if container_info.status == "running":  # type: ignore
-            break
-        sleep(1)
-
-    return container  # type: ignore
-
 
 def get_latest_event(events: dict[datetime, Any]) -> Any:
     return events[max(events.keys())]
